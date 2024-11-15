27e2aec0420be4a6db4f86380e399a1449a7f936
hadi <mc0965490@gmail.com>
2024-11-15 22:42:14 +0430

    Add files via upload

---
+++ about.php
@@ -0,0 +1,8 @@
+<?php
+include("header.html");
+?>
+    <p class="sb">کانال تلگرام فروشگاه : motor.mt</p>
+    <p class="sb">آی دی سرور دیسکورد فروشگاه : motor.mt.net</p>
+<?php
+include("footer.html");
+?>
