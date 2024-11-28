0b6b348f57860d613d6f7a0c1da36481a6d12fb6
hadi <mc0965490@gmail.com>
2024-11-28 13:55:16 +0430

    Add files via upload

--- index.php
+++ index.php
@@ -5,6 +5,17 @@ include("header.html");
     <h4>فروش انواع لوازم تعمیری ، مصرفی ،اسپرتی موتور </h4>
     <h1>آموزش ها </h1>
     <h4>آموزش تعمیر و تقویت اصولی موتور هندا به صورت تخصصی و حرفه ای </h4>
+    <video controls width="200">
+                    <source src="video/test.mp4" type="video/webm" />
+                  
+                    <source src="video/test.mp4" type="video/mp4" />
+                  
+                    Download the
+                    <a href="video/test.mp4">WEBM</a>
+                    or
+                    <a href="video/test.mp4">MP4</a>
+                    video.
+                  </video>
 <?php
 include("footer.html");
 ?>
